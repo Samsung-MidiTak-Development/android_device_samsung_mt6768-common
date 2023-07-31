a67b617d7d07a2892fba45cf81899b7a4466396f
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-07-31 21:19:16 +0700

    mt6768-common: Build fingerprint HAL
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
