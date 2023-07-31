213ab75233c27553f9826e039b112adcf2bdef26
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-07-31 20:23:03 +0700

    mt6768-common: Add empty sepolicy folder
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
