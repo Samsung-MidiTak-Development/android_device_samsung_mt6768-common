9dab8c738ca4837e99b05342f49f232b76d5dcb3
SamarV-121 <samarvispute121@pm.me>
2023-08-02 21:00:32 +0700

    mt6768-common: Add overlay for vibration intensity levels

--- mt6768.mk
+++ mt6768.mk
@@ -139,6 +139,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    SettingsOverlay \
     FrameworkResOverlay \
     WifiOverlay \
     TetheringConfigOverlay
