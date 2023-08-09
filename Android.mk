db4514f39554379cd933df9d4007026701fbab3f
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-08-09 22:00:09 +0700

    mt6768-common: Properly create graphics symlinks
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,14 +8,28 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter a32, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-VULKAN_SYMLINKS := $(TARGET_OUT_VENDOR)
-$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating vulkan symlinks: $@"
-	@mkdir -p $@/lib/hw/
-	@mkdir -p $@/lib64/hw/
-	@ln -sf $@/lib/egl/libGLES_mali.so $@/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	@ln -sf $@/lib64/egl/libGLES_mali.so $@/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+GRAPHICS_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib \
+    $(TARGET_OUT_VENDOR)/lib64 \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
 
-ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINKS)
+$(GRAPHICS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Creating graphics symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf /vendor/lib64/mt6768/libdpframework.so $(TARGET_OUT_VENDOR)/lib64/libdpframework.so
+	@ln -sf /vendor/lib64/mt6768/libmtk_drvb.so $(TARGET_OUT_VENDOR)/lib64/libmtk_drvb.so
+	@ln -sf /vendor/lib64/mt6768/libnir_neon_driver.so $(TARGET_OUT_VENDOR)/lib64/libnir_neon_driver.so
+	@ln -sf /vendor/lib64/mt6768/libpq_prot.so $(TARGET_OUT_VENDOR)/lib64/libpq_prot.so
+	@ln -sf /vendor/lib/mt6768/libdpframework.so $(TARGET_OUT_VENDOR)/lib/libdpframework.so
+	@ln -sf /vendor/lib/mt6768/libmtk_drvb.so $(TARGET_OUT_VENDOR)/lib/libmtk_drvb.so
+	@ln -sf /vendor/lib/mt6768/libnir_neon_driver.so $(TARGET_OUT_VENDOR)/lib/libnir_neon_driver.so
+	@ln -sf /vendor/lib/mt6768/libpq_prot.so $(TARGET_OUT_VENDOR)/lib/libpq_prot.so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GRAPHICS_SYMLINKS)
 
 endif
